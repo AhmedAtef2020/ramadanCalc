76403d0404381f4374cc363ab682430997e28796
AhmedAtef <ahmedatef1428@gmail.com>
2023-02-20 15:19:32 +0200

    first version of my Ramadn project

---
+++ README.md
@@ -0,0 +1,18 @@
+# Ramadan Days Calculating App
+* * This was done by Ahmed Atef from a youtube course for Eng. Mohamed Abusrea
+
+Images by freepik.com 
+
+## The project 
+the project counts the day in ramadan (if we are in ramadn) or days to ramadn (if we are out ramadn)
+
+### libiraries:
+- I used in CSS:
+    - normalize.css
+
+- I used in JS:
+    - moment-hijri.js
+    - moment.js
+
+    best wishes
+    Ahmed Atef
---
+++ script/mainCalc.js
@@ -0,0 +1,28 @@
+// global variables
+const remainingDaysEle = document.querySelector(".remainingDays");
+const todatDate = moment();
+const todayHijryFormat = todatDate.format("iYYYY/iM/iD");
+const todayHijryDate = moment(todayHijryFormat, "iYYYY/iM/iD");
+const currentHijriYear = parseInt(todatDate.format("iYYYY"));
+let ramadanStart = moment(`${currentHijriYear}/9/1`, "iYYYY/iM/iD");
+let inRamadan = ramadanStart.daysInMonth();
+let diff = todayHijryDate.diff(ramadanStart, "days");
+let ramadanCase = document.getElementById("ramadanCase");
+
+
+// if statment 
+if (diff >= 0) {
+    if (diff <= inRamadan) {//in ramadan
+        ramadanCase.textContent = "في";
+        diff = inRamadan - diff;
+    }
+    else {//after ramadan
+        ramadanCase.textContent = "على";
+        const nextHijriYear = currentHijriYear + 1;
+        ramadanStart = moment(`${nextHijriYear}/9/1`, "iYYYY/iM/iD");
+        diff = todayHijryDate.diff(ramadanStart, "days");
+    }
+}
+//before ramadan
+ramadanCase.textContent = "على";
+remainingDaysEle.textContent = Math.abs(diff).toLocaleString("ar-sa");
